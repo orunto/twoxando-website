78c7a94602355746eba8e50e22cbe5170b95d0f9
Orunto Eniola <oruntoeniola@aol.com>
2022-11-03 15:34:36 +0100

    All sections

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -22,6 +22,19 @@ const Home = () => {
                 <p>Lagos Dogs os a story driven, third person shooter following  a trail of violent crimes and cybercimes in Lagos city</p>
                 <button>Learn More</button>
             </section>
+            <section id='experience'>
+                <h1>EXPERIENCE LAGOS</h1>
+                <p>Coming to PC and consoles</p>
+                <button>Our Process</button>
+            </section>
+            <section id='onourway'>
+                <h1>ON OUR WAY</h1>
+                <p>Lagos dogs is in it’s early stages of developmentand in  abid to carry our fans along in the process we’ve created an updates list</p>
+                <form>
+                    <input type='email' name='email' id='email' placeholder='johndoe@example.com' />
+                    <button type='submit'>Stay in the loop</button>
+                </form>
+            </section>
         </div>
     )
 }
